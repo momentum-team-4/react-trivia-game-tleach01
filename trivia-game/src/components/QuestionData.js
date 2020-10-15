efde6080754d1db3cb7e7a64f7201fdef32bfed7
tleach01 <taylorleach0@gmail.com>
2020-10-15 08:01:25 -0400

    questions showing, added next and previous question button

--- trivia-game/src/components/QuestionData.js
+++ trivia-game/src/components/QuestionData.js
@@ -10,22 +10,45 @@ import React, { useState, useEffect } from 'react'
 export default function QuestionData (props) {
   const { category } = props
   const [triviaQuestions, setTriviaQuestions] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     setTriviaQuestions([])
-    if (!category) {return}
+    if (!category) { return }
     fetch('https://opentdb.com/api.php?amount=10&category=' + category.id)
       .then(response => response.json())
       .then(data => {
         console.log(data)
-
+        const questions = data.results.map(question => {
+          return {
+            question: question.question,
+            correct_answer: question.correct_answer
+          }
+        })
+        setTriviaQuestions(questions)
       })
   }, [category])
+
+  let question
+  if (triviaQuestions.length > 0) {
+    question = triviaQuestions[currentIndex]
+  }
 
   return (
     <div className='QuestionData'>
       <h2>{category.name}</h2>
-      <div>  </div>
+
+      {question && (
+        <div> Question: {question.question} </div>
+      )}
+
+      <div>
+        {currentIndex > 0 &&
+          <button onClick={() => setCurrentIndex(currentIndex - 1)}>Previous Question</button>}
+        {' '}
+        {currentIndex < triviaQuestions.length - 1 &&
+          <button onClick={() => setCurrentIndex(currentIndex + 1)}>Next Question</button>}
+      </div>
 
     </div>
   )
